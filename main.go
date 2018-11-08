5c7317b8b249f3d820ea855574243ca657b57a6e
hirokisan <expsharetoworld@gmail.com>
2018-11-08 08:40:31 +0000

    Use input, query, form

--- main.go
+++ main.go
@@ -9,5 +9,27 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/user/:name/", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(200, gin.H{
+			"message": name,
+		})
+	})
+	r.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "guest")
+		lastname := c.Query("lastname")
+		c.JSON(200, gin.H{
+			"message":   "OK",
+			"firstname": firstname,
+			"lastname":  lastname,
+		})
+	})
+	r.POST("/user/", func(c *gin.Context) {
+		name := c.PostForm("name")
+		c.JSON(200, gin.H{
+			"message": "OK",
+			"name":    name,
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# sample-gin
+
+## Todo
+* [ ] Use gorm
